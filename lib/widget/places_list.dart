bda3048d9a9d054a53f48bc3e822b7660f9f732f
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-10-08 19:28:03 +0200

    add basic setup

---
+++ lib/widget/places_list.dart
@@ -0,0 +1,40 @@
+import 'package:favorites_place/models/place.dart';
+import 'package:favorites_place/screens/place_details.dart';
+import 'package:flutter/material.dart';
+
+class PlacesList extends StatelessWidget {
+  const PlacesList({super.key, required this.places});
+  final List<Place> places;
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return Center(
+          child: Text(
+        'No places add yet!',
+        style: Theme.of(context)
+            .textTheme
+            .titleMedium!
+            .copyWith(color: Theme.of(context).colorScheme.onSurface),
+      ));
+    }
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (ctx, index) => ListTile(
+        title: Text(
+          places[index].title,
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium!
+              .copyWith(color: Theme.of(context).colorScheme.onSurface),
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailsScreen(place: places[index]),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
